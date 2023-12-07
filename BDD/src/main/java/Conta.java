faefc759324707933e9a2529983ed338d90bf94e
candizz299 <113790761+candizz299@users.noreply.github.com>
2023-12-06 21:34:21 -0300

    Add files via upload

---
+++ BDD/src/main/java/Conta.java
@@ -0,0 +1,50 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Conta {
+
+	@Given("um cliente especial com saldo atual de {int} reais")
+	public void um_cliente_especial_com_saldo_atual_de_reais(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("for solicitado um saque no valor de {int} reais")
+	public void for_solicitado_um_saque_no_valor_de_reais(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("deve efetuar o saque e atualizar o saque da conta para {int} reais")
+	public void deve_efetuar_o_saque_e_atualizar_o_saque_da_conta_para_reais(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("check more outcomes")
+	public void check_more_outcomes() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("um cliente comun com saldo atual de {int} reais")
+	public void um_cliente_comun_com_saldo_atual_de_reais(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("solicitar um saque de {int} reais")
+	public void solicitar_um_saque_de_reais(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("não deve efetuar o saque e deve retornar a mensagem saldo insuficiente")
+	public void não_deve_efetuar_o_saque_e_deve_retornar_a_mensagem_saldo_insuficiente() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	
+}
